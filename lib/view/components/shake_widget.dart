f572a24775bb7f6b0ca30d476b5a048e370ebab4
Walid Harkous <harkousw@wit.edu>
2023-03-19 13:19:07 -0400

    Cleaned up animations, and updated some ui elements in task_view

---
+++ lib/view/components/shake_widget.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+class ShakeWidget extends StatefulWidget {
+  const ShakeWidget({
+    Key? key,
+    this.duration = const Duration(milliseconds: 300),
+    this.deltaX = 4,
+    this.deltaY = 4,
+    this.curve = Curves.linear,
+    this.enabled = false,
+    required this.child,
+  }) : super(key: key);
+
+  final Duration duration;
+  final double deltaX;
+  final double deltaY;
+  final Widget child;
+  final Curve curve;
+  final bool enabled;
+
+  @override
+  _ShakeWidgetState createState() => _ShakeWidgetState();
+}
+
+class _ShakeWidgetState extends State<ShakeWidget>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      duration: widget.duration,
+      vsync: this,
+    )
+      ..forward()
+      ..addListener(() {
+        if (controller.isCompleted) {
+          controller.reverse();
+        } else if (controller.isDismissed) {
+          controller.forward();
+        }
+      });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  /// convert 0-1 to 0-1-0
+  double shake(double value) =>
+      1 * (0.5 - (0.5 - widget.curve.transform(value)).abs());
+
+  @override
+  Widget build(BuildContext context) {
+    if (!widget.enabled) {
+      return widget.child;
+    }
+
+    final animation = Tween<double>(begin: -2.0, end: 2.0).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: widget.curve,
+      ),
+    );
+
+    return AnimatedBuilder(
+      animation: controller,
+      builder: (context, child) => Transform.rotate(
+        angle: animation.value * (math.pi / 180),
+        child: child,
+      ),
+      child: widget.child,
+    );
+  }
+}
--- lib/view/components/tasks_view.dart
+++ lib/view/components/tasks_view.dart
@@ -4,6 +4,7 @@ import 'package:taskmate/model/profile.dart';
 import 'package:taskmate/model/todo_list.dart';
 import 'package:taskmate/services/globals.dart';
 import 'package:taskmate/services/service.dart';
+import 'package:taskmate/view/components/shake_widget.dart';
 
 class TasksView extends StatefulWidget {
   TodoList list;
@@ -15,10 +16,16 @@ class TasksView extends StatefulWidget {
 
 class _TasksViewState extends State<TasksView> {
   Profile profile = Get.find<Profile>();
+  late List<bool> pressedTasks;
+  @override
+  void initState() {
+    pressedTasks = List.generate(widget.list.tasks.length, (index) => false);
+  }
 
   @override
   Widget build(BuildContext context) {
     final _width = MediaQuery.of(context).size.width;
+
     final Column tasks = Column(
         children: List.generate(widget.list.tasks.length, (index) {
       //? current task in index & the index of the todo list
@@ -34,56 +41,93 @@ class _TasksViewState extends State<TasksView> {
                   ? _width / 2
                   : _width / 3;
 
-      return Row(
-        children: [
-          IconButton(
-              onPressed: () {
-                setState(() {
-                  profile.todoLists[listIndex].tasks[index].isDone =
-                      !profile.todoLists[listIndex].tasks[index].isDone;
-                  MyServices.updateTodoList(widget.list);
-                });
-              },
-              icon: Icon(
-                task.isDone
-                    ? Icons.radio_button_checked_rounded
-                    : Icons.radio_button_unchecked_rounded,
-                size: 18,
-                color: list.color,
-              )),
-          Flexible(
-            child: Stack(
-              alignment: Alignment.centerLeft,
-              children: [
-                Text(
-                  task.name,
-                  maxLines: 1,
-                  softWrap: false,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                AnimatedPositioned(
-                  duration: Duration(
-                      milliseconds: (task.name.length < 20) ? 250 : 550),
-                  left: 0,
-                  right: task.isDone ? 0 : rightPosition,
-                  child: Container(
-                    height: 1.4,
-                    decoration: const BoxDecoration(color: MyColors.uiButton),
+      return InkWell(
+        onTap: () {
+          setState(() {
+            profile.todoLists[listIndex].tasks[index].isDone =
+                !profile.todoLists[listIndex].tasks[index].isDone;
+            MyServices.updateTodoList(widget.list);
+          });
+        },
+        onLongPress: () {
+          setState(() {
+            pressedTasks[index] = !pressedTasks[index];
+          });
+        },
+        child: Row(
+          children: [
+            Padding(
+                padding: const EdgeInsets.all(10),
+                child: pressedTasks[index]
+                    ? InkWell(
+                        onTap: () {
+                          setState(() {
+                            profile.todoLists[listIndex].tasks.removeAt(index);
+                            pressedTasks[index] = false;
+                            MyServices.updateTodoList(list);
+                          });
+                        },
+                        child: const Icon(
+                          Icons.close_rounded,
+                          color: MyColors.uiButton,
+                          size: 20,
+                        ),
+                      )
+                    : Icon(
+                        task.isDone
+                            ? Icons.radio_button_checked_rounded
+                            : Icons.radio_button_unchecked_rounded,
+                        size: 18,
+                        color: list.color,
+                      )),
+            Flexible(
+              child: Stack(
+                alignment: Alignment.centerLeft,
+                children: [
+                  ShakeWidget(
+                    enabled: pressedTasks[index],
+                    child: Text(
+                      task.name,
+                      maxLines: 1,
+                      softWrap: false,
+                      overflow: TextOverflow.ellipsis,
+                    ),
                   ),
-                ),
-              ],
+                  AnimatedPositioned(
+                    duration: Duration(
+                        milliseconds: (task.name.length < 20) ? 350 : 550),
+                    left: 0,
+                    right: task.isDone ? 0 : rightPosition,
+                    child: Container(
+                      height: 1.4,
+                      decoration: const BoxDecoration(color: MyColors.uiButton),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       );
     }));
-    return Container(
-      color: MyColors.bg,
-      child: Padding(
-          padding: const EdgeInsets.all(8),
-          child: SingleChildScrollView(
-            child: tasks,
-          )),
+    return Stack(
+      fit: StackFit.expand,
+      alignment: Alignment.center,
+      children: [
+        Opacity(
+          opacity: 0.2,
+          child: Icon(
+            widget.list.icon,
+            color: widget.list.color,
+            size: 150,
+          ),
+        ),
+        Padding(
+            padding: const EdgeInsets.all(8),
+            child: SingleChildScrollView(
+              child: tasks,
+            )),
+      ],
     );
   }
 }
